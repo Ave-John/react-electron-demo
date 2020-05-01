f9deedf0ce4e7336ca96243780d3f57e9cdf2f71
Ave-John <64627930+Ave-John@users.noreply.github.com>
2020-05-01 12:24:06 +0530

    Updates

---
+++ precache-manifest.824dc78e83e3371692195c90259c75bc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543364cc3ab0e3628f2a9f1ea63b6ae2",
+    "url": "/react-electron-demo/index.html"
+  },
+  {
+    "revision": "8977cb8b1bef4f61ec09",
+    "url": "/react-electron-demo/static/css/2.8cf3568c.chunk.css"
+  },
+  {
+    "revision": "90ed48d3f00ecf1c8aa9",
+    "url": "/react-electron-demo/static/css/main.89cc6f32.chunk.css"
+  },
+  {
+    "revision": "8977cb8b1bef4f61ec09",
+    "url": "/react-electron-demo/static/js/2.ec5d3ea5.chunk.js"
+  },
+  {
+    "revision": "394df49ea797b2253ca54779e4d12200",
+    "url": "/react-electron-demo/static/js/2.ec5d3ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ed48d3f00ecf1c8aa9",
+    "url": "/react-electron-demo/static/js/main.583a8da8.chunk.js"
+  },
+  {
+    "revision": "9536e363dbd4c7c8aae1",
+    "url": "/react-electron-demo/static/js/runtime-main.db764d06.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/react-electron-demo/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "34ed7d3e883a4d5ff93810f4057212be",
+    "url": "/react-electron-demo/static/media/dashboard.34ed7d3e.jpg"
+  },
+  {
+    "revision": "7ee6892c5faa9ab4f980fede5eb466db",
+    "url": "/react-electron-demo/static/media/edit.7ee6892c.png"
+  },
+  {
+    "revision": "0a7a8e01e592a5dfe8b548a15f0106ae",
+    "url": "/react-electron-demo/static/media/filter.0a7a8e01.jpg"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/react-electron-demo/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/react-electron-demo/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "2d2afb2719a1ee903e576e7c457daf81",
+    "url": "/react-electron-demo/static/media/primeicons.2d2afb27.eot"
+  },
+  {
+    "revision": "66ee0deb739ca71f0ecdc39d7c1b22cb",
+    "url": "/react-electron-demo/static/media/primeicons.66ee0deb.woff"
+  },
+  {
+    "revision": "df0140f8e79ecfeffaf85220aaecd7c4",
+    "url": "/react-electron-demo/static/media/primeicons.df0140f8.ttf"
+  },
+  {
+    "revision": "e5e0e94474d5fd92e7e800a8865d297c",
+    "url": "/react-electron-demo/static/media/primeicons.e5e0e944.svg"
+  }
+]);
